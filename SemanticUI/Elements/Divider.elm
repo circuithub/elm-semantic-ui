1c5396008ba503ecd3b1e4749323da13de61b8b2
Ollie Charles <ollie@ocharles.org.uk>
2017-06-02 13:54:10 +0100

    Add SemanticUI.Elements.Divider

--- SemanticUI/Elements/Divider.elm
+++ SemanticUI/Elements/Divider.elm
@@ -1,9 +1,19 @@
-module SemanticUI.Elements.Divider exposing (..)
+module SemanticUI.Elements.Divider exposing (divider)
+
+{-|
+A divider visually segments content into groups.
+
+# Viewing dividers
+
+@docs divider
+-}
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+{-| View a horizontal divider, optionally containing some centered text.
+-}
 divider : Maybe String -> Html msg
 divider text =
     div
