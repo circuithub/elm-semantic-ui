ffb1d3a6b582100763d856599e72cb334905512b
Ollie Charles <ollie@ocharles.org.uk>
2017-06-02 14:42:13 +0100

    Add SemanticUI.Collections.Form

--- SemanticUI/Collections/Form.elm
+++ SemanticUI/Collections/Form.elm
@@ -1,34 +1,69 @@
-module SemanticUI.Collections.Form exposing (..)
+module SemanticUI.Collections.Form
+    exposing
+        ( Config
+        , init
+        , attributes
+        , error
+        , form
+        )
+
+{-| A form displays a set of related user input fields in a structured way.
+
+# Viewing forms
+
+@docs form
+
+# Form properties
+
+@docs init, Config, attributes
+
+## Errors
+
+If a form is in an error state, it will automatically show any error message blocks.
+
+@docs error
+-}
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
-type alias Model msg =
+{-| The configuration of a form.
+-}
+type alias Config msg =
     { attributes : List (Attribute msg)
     , error : Bool
     }
 
 
-init : Model msg
+{-| The simplest configuration of a form. Corresponds to `class="ui form"`.
+-}
+init : Config msg
 init =
     { attributes = []
     , error = False
     }
 
 
-attributes : List (Attribute msg) -> Model msg -> Model msg
+{-| Any other custom `Attribute`s to add to this form. Custom attributes
+ will be added before `elm-semantic-ui` attributes.
+-}
+attributes : List (Attribute msg) -> Config msg -> Config msg
 attributes attributes model =
     { model | attributes = attributes }
 
 
-error : Bool -> Model msg -> Model msg
+{-| Flag whether or not this form contains errors.
+-}
+error : Bool -> Config msg -> Config msg
 error error model =
     { model | error = error }
 
 
-view : Model msg -> List (Html msg) -> Html msg
-view { attributes, error } contents =
+{-| View a form as a `<form>` tag.
+-}
+form : Config msg -> List (Html msg) -> Html msg
+form { attributes, error } contents =
     Html.form
         (List.concat
             [ attributes
