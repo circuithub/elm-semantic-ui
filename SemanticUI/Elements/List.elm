8967148f1f9d72b6369bae9a4271cef4cc779037
Ollie Charles <ollie@ocharles.org.uk>
2017-06-23 14:04:15 +0100

    Overhaul SemanticUI.Elements.List

--- SemanticUI/Elements/List.elm
+++ SemanticUI/Elements/List.elm
@@ -1,4 +1,15 @@
-module SemanticUI.Elements.List exposing (ul, item, ListItem)
+module SemanticUI.Elements.List
+    exposing
+        ( ul
+        , item
+        , ListItem
+        , Config
+        , init
+        , relaxed
+        , divided
+        , div
+        , attributes
+        )
 
 {-|
 A list is used to group related content.
@@ -26,6 +37,36 @@ type ListItem msg
     = ListItem (List (Html msg))
 
 
+type alias Config msg =
+    { relaxed : Bool
+    , divided : Bool
+    , attributes : List (Attribute msg)
+    }
+
+
+init : Config msg
+init =
+    { relaxed = False
+    , divided = False
+    , attributes = []
+    }
+
+
+relaxed : Bool -> Config msg -> Config msg
+relaxed relaxed config =
+    { config | relaxed = relaxed }
+
+
+divided : Bool -> Config msg -> Config msg
+divided divided config =
+    { config | divided = divided }
+
+
+attributes : List (Attribute msg) -> Config msg -> Config msg
+attributes attributes config =
+    { config | attributes = attributes }
+
+
 {-| Wrap Html as a list item.
 -}
 item : List (Html msg) -> ListItem msg
@@ -35,6 +76,28 @@ item =
 
 {-| View a list of items as a `<ul>` element.
 -}
-ul : List (ListItem msg) -> Html msg
-ul items =
-    Html.ul [ class "ui list" ] (List.map (\(ListItem contents) -> li [] contents) items)
+ul : Config msg -> List (ListItem msg) -> Html msg
+ul =
+    view Html.ul
+
+
+{-| View a list of items as a `div` element.
+-}
+div : Config msg -> List (ListItem msg) -> Html msg
+div =
+    view Html.div
+
+
+view element { attributes, relaxed, divided } items =
+    element
+        (List.concat
+            [ attributes
+            , [ class "ui list"
+              , classList
+                    [ ( "relaxed", relaxed )
+                    , ( "divided", divided )
+                    ]
+              ]
+            ]
+        )
+        (List.map (\(ListItem i) -> Html.div [ class "item" ] i) items)
