9917adf9ababb9e98e543261520d1d209257eb1d
Ollie Charles <ollie@ocharles.org.uk>
2017-10-06 12:07:04 +0100

    Embed all modals in scrolling dimmables by default

--- SemanticUI/Modules/Modal.elm
+++ SemanticUI/Modules/Modal.elm
@@ -34,7 +34,7 @@ view { fullscreen } body =
             div [ class "ui page dimmer transition" ] []
 
         Just { header, content, close, actions } ->
-            div [ class "ui active page dimmer transition" ]
+            div [ class "ui active page dimmer transition scrolling" ]
                 [ div [ class "ui active visible modal transition"
                       , classList [("fullscreen", fullscreen)]]
                     (List.concat
